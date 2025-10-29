93e686af1a46094cbe6543223c14e79bdf5218d5
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-10-29 15:19:41 +0530

    add: Challenge: Travel Journal - Header

--- section-two/src/main.jsx
+++ section-two/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from "react-dom/client"
 import './index.css'
-import App from './App.jsx'
+/**
+ * Challenge: Travel Journal - Header
+ * 
+ * - Create an App component in a separate file.
+ *   Import it here and render it
+ * - Create a "components" folder and a Header component.
+ *   Render the Header inside the App component.
+ * - Follow the Travel Journal design to build the Header
+ *   for our page.
+ */
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"))
+root.render(/* Render App component here*/)
