8c866425a8e0e3ae3e1a907ec0beda8af9b41eb1
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-01 20:30:19 +0530

    add: Non-string props challenge

--- section-two/props-practice/App.jsx
+++ section-two/props-practice/App.jsx
@@ -1,21 +1,13 @@
 /**
- * Challenge: create a page that displays your favorite jokes
- * - Create a Joke component in its own file.
- * - Import and render 4-5 <Joke /> components
- * - Each Joke should receive a "setup" prop and a "punchline" prop
- *   and render those however you'd like
- * - Use your favorite 2-part jokes (setup & punchline), or check
- *   jokes.md file for some examples.
- *
- * EXTRA CREDIT:
- * Some jokes are only a punchline with no setup:
- *
- * E.g.: "It’s hard to explain puns to kleptomaniacs because
- * they always take things literally."
- *
- * If you don't pass in a "question" prop, how might you make it only
- * show the punchline?
+ * Non-string props Exercise #1 Challenge: Think critically - how would you pass in a prop that 
+ * isn't a string datatype.
+ * 
+ * E.g. Say you want each Joke component to receive an "upvotes" 
+ * and "downvotes" prop that is a number, as well as a prop with 
+ * an array of comments, and a boolean of whether the joke is a 
+ * pun (`isPun`).
  */
+
 import Joke from "./Joke";
 
 export default function App() {
