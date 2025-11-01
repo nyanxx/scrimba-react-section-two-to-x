46220fea05ae7f14b1ebe0aaa3996404b9355c68
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-01 18:24:20 +0530

    done: Destructuring props challenge

--- section-two/props/Contact.jsx
+++ section-two/props/Contact.jsx
@@ -3,18 +3,18 @@
  * destructured the props object
  */
 
-export default function Contact({ img, name, phone, email }) {
+export default function Contact({ img, imgAlt, name, phoneNo, email }) {
   return (
     <article className="contact-card">
-      <img src={props.img} alt={props.imgAlt} />
-      <h3>{props.name}</h3>
+      <img src={img} alt={imgAlt} />
+      <h3>{name}</h3>
       <div className="info-group">
         <img src="./images/phone-icon.png" alt="phone icon" />
-        <p>{props.phoneNo}</p>
+        <p>{phoneNo}</p>
       </div>
       <div className="info-group">
         <img src="./images/mail-icon.png" alt="mail icon" />
-        <p>{props.email}</p>
+        <p>{email}</p>
       </div>
     </article>
   );
