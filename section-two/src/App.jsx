3b32154d73819db839ad2a8e14b2e07dcdde5990
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-01 12:00:52 +0530

    myadd

--- section-two/src/App.jsx
+++ section-two/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import Entry from "./components/Entry";
 import MainContent from "./components/MainContent";
 
 export default function App() {
   return (
     <main className="app">
       <Header />
-      <MainContent />
+      {/* <MainContent /> */}
+      <Entry />
     </main>
   );
 }
