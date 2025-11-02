ac1789fa408bfddee23553182c32aee51ed3f029
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-02 21:14:10 +0530

    myadd: wip - Non-string props challenge

--- section-two/props-practice/App.jsx
+++ section-two/props-practice/App.jsx
@@ -1,10 +1,10 @@
 /**
- * Non-string props Exercise #1 Challenge: Think critically - how would you pass in a prop that 
+ * Non-string props Exercise #1 Challenge: Think critically - how would you pass in a prop that
  * isn't a string datatype.
- * 
- * E.g. Say you want each Joke component to receive an "upvotes" 
- * and "downvotes" prop that is a number, as well as a prop with 
- * an array of comments, and a boolean of whether the joke is a 
+ *
+ * E.g. Say you want each Joke component to receive an "upvotes"
+ * and "downvotes" prop that is a number, as well as a prop with
+ * an array of comments, and a boolean of whether the joke is a
  * pun (`isPun`).
  */
 
@@ -18,19 +18,35 @@ export default function App() {
       {/* only punchline */}
       <Joke
         punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+        upvotes={1}
+        downvotes={4}
+        comments={["It's funny", "Am i the only one who didn't get this!"]}
+        isPun={true}
       />
       {/* setup and punchline */}
       <Joke
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
+        upvotes={4}
+        downvotes={1}
+        comments={["LOL", "jajajajajaja"]}
+        isPun={true}
       />
       <Joke
         setup="How did the hacker escape the police?"
         punchline="He just ransomware!"
+        upvotes="9"
+        downvotes="2"
+        comments={["Ran some where hahaha", "Nice one"]}
+        isPun="true"
       />
       <Joke
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
+        upvotes={5}
+        downvotes={1}
+        comments={["I see what you did there!", "nice", "noice👍"]}
+        isPun={true}
       />
     </main>
   );
--- section-two/props-practice/Joke.jsx
+++ section-two/props-practice/Joke.jsx
@@ -1,11 +1,19 @@
 export default function Joke(props) {
-  console.log(props);
   return (
     <article className="joke">
       {props.setup && <p className="setup">"{props.setup}"</p>}
       {/* another way */}
       {/* <p style={{display: props.setup ? "block" : "none"}} className="setup">"{props.setup}"</p> */}
       <p>"{props.punchline}"</p>
+      <div className="votes">
+        <span className="upvotes">{props.upvotes}👍</span>
+        <span className="downvotes">{props.downvotes}👎</span>
+      </div>
+      <section className="comment-section">
+        {props.comments.map((indiComment) => (
+          <span className="individual-comment">{indiComment}</span>
+        ))}
+      </section>
     </article>
   );
 }
