516f46342d7d6df38c0f199aff6d5a6302f9cf5b
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-02 21:46:44 +0530

    add: Challenge: pass props to the Entry component to display

--- section-two/src/App.jsx
+++ section-two/src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
 import Entry from "./components/Entry";
-import MainContent from "./components/MainContent";
+
+/**
+ * Challenge: pass props to the Entry component to display
+ * its data. See the `data.md` file for each prop name and its
+ * value.
+ *
+ * Then on the Entry component, receive and display the values
+ * for those props. In the end, the page should look the same
+ * as it does now, but without all the hard-coded data in the
+ * component
+ */
 
 export default function App() {
   return (
     <main className="app">
       <Header />
-      {/* <MainContent /> */}
       <Entry />
     </main>
   );
