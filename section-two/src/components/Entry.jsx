b9ade24625fee591b33a37e7897c7a661b0b56e3
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-03 08:40:14 +0530

    myadd: wip - Challenge: pass props to the Entry component to display

--- section-two/src/components/Entry.jsx
+++ section-two/src/components/Entry.jsx
@@ -1,25 +1,16 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <section className="entry">
-      <img
-        src="https://scrimba.com/links/travel-journal-japan-image-url"
-        alt="mount fuji"
-      />
+      <img src={props.img.src} alt={props.img.alt} />
       <article>
         <div>
           <img src="./src/assets/location.svg" alt="Location marker" />
-          <span>J A P A N</span>
-          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-            View on Google Maps
-          </a>
+          <span>{props.country}</span>
+          <a href={props.googleMapsLink}>View on Google Maps</a>
         </div>
-        <h2>Mount Fuji</h2>
-        <span>12 Jan, 2021 - 24 Jan, 2021</span>
-        <p>
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h2>{props.title}</h2>
+        <span>{props.dates}</span>
+        <p>{props.text}</p>
       </article>
     </section>
   );
