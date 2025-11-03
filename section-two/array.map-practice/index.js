6719f200615904a82cdf680ab5b9fec2b214b923
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-03 10:48:55 +0530

    done: array.map-practice

--- section-two/array.map-practice/index.js
+++ section-two/array.map-practice/index.js
@@ -5,7 +5,10 @@ Given an array of numbers, return an array of each number, squared
 const nums = [1, 2, 3, 4, 5];
 // -->       [1, 4, 9, 16, 25]
 // Your code here
-console.log(nums.map((num) => num * num));
+const squares = nums.map(function (num) {
+  return num * num;
+});
+console.log(squares);
 
 /*
 Challenge 2:
@@ -16,8 +19,10 @@ the first letter of each string is capitalized
 const names = ["alice", "bob", "charlie", "danielle"];
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
-
-console.log(names.map((name) => name.charAt(0).toUpperCase() + name.slice(1)));
+const capitalized = names.map((name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+});
+console.log(capitalized);
 
 /*
 Challenge 3:
@@ -28,9 +33,9 @@ E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
 return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 */
 
-const pokemons = ["Bulbasaur", "Charmander", "Squirtle"];
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
 // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 // Your code here
-console.log(
-  pokemons.map((pokemon, index) => `<p key="${index}">${pokemon}</p>`)
-);
+const pokemonInP = pokemon.map((mon, index) => `<p key="${index}">${mon}</p>`);
+
+console.log(pokemonInP);
