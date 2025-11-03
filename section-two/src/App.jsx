dd74987837932ff4aef666af2692c1f368c2ae5c
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-03 12:50:39 +0530

    add: Travel Journal: Map Entry components challenge

--- section-two/src/App.jsx
+++ section-two/src/App.jsx
@@ -1,6 +1,15 @@
 import Header from "./components/Header";
 import Entry from "./components/Entry";
 
+/**
+ * Travel Journal: Map Entry components challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
 export default function App() {
   return (
     <main className="app">
