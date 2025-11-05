6b6c0a5d6c68afc6cc6d2189be13d060210794f2
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-05 16:44:39 +0530

    done: Event Listeners Challenge 2

--- section-three-react-state/practice-exercise/event-listeners/index.jsx
+++ section-three-react-state/practice-exercise/event-listeners/index.jsx
@@ -1,25 +1,29 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 function App() {
-  
   function handleClick() {
-    console.log("I was clicked!")
+    console.log("I was clicked!");
   }
-  
+
+  function handleMouseOver() {
+    console.log("Get that cursor off me!!!");
+  }
+
   /**
    * Challenge: Log something to the console when the mouse
    * hovers over the image
    */
-  
+
   return (
     <main className="container">
       <img
         src="https://picsum.photos/640/360"
         alt="Placeholder image from Picsum"
+        onMouseOver={handleMouseOver}
       />
       <button onClick={handleClick}>Click me</button>
     </main>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
