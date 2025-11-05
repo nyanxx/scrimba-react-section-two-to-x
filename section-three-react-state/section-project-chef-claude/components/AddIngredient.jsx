1cdcb8aea91f1bda294903ca0df306dbcb17736b
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-05 16:50:35 +0530

    add: Chef Claude: Map ingredients list Review Challenge

--- section-three-react-state/section-project-chef-claude/components/AddIngredient.jsx
+++ section-three-react-state/section-project-chef-claude/components/AddIngredient.jsx
@@ -1,23 +1,34 @@
 export default function AddIngredient() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  /**
+   * Chef Claude: Map ingredients list Review Challenge:
+   * Map over the list of ingredients and render them as list items
+   *
+   * Note: We're doing things a weird way here. Don't worry,
+   * we're building up to learning the right way 🙂
+   */
   return (
-    <form className="add-ingredient">
-      <input
-        id="input-ingredient"
-        name="input-ingredient"
-        aria-label="Enter ingredient"
-        type="text"
-        className="input-ingredient"
-        placeholder="e.g. oregano"
-      />
-      <button
-        id="btn-add-ingredient"
-        aria-label="Add ingredient"
-        name="btn-add-ingredient"
-        className="btn-add-ingredient"
-      >
-        Add ingredient
-      </button>
-      {/* <button className="btn-add-ingredient">+ Add Ingredient</button> */}
-    </form>
+    <>
+      <form className="add-ingredient">
+        <input
+          id="input-ingredient"
+          name="input-ingredient"
+          aria-label="Enter ingredient"
+          type="text"
+          className="input-ingredient"
+          placeholder="e.g. oregano"
+        />
+        <button
+          id="btn-add-ingredient"
+          aria-label="Add ingredient"
+          name="btn-add-ingredient"
+          className="btn-add-ingredient"
+        >
+          Add ingredient
+        </button>
+      </form>
+      <ul></ul>
+    </>
   );
 }
