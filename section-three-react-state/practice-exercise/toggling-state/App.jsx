e59c9de45395bef5a6f4f23c1b58adb424470c50
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-06 15:55:34 +0530

    done: Toggling State Challenge

--- section-three-react-state/practice-exercise/toggling-state/App.jsx
+++ section-three-react-state/practice-exercise/toggling-state/App.jsx
@@ -7,19 +7,25 @@ export default function App() {
    * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
    */
 
-  const [state, setState] = React.useState(true);
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
 
   function handleClick() {
-    setState((prevState) => {
-      return !prevState;
+    setIsGoingOut((prevDecision) => {
+      return !prevDecision;
     });
   }
 
   return (
     <main>
       <h1 className="title">Do I feel like going out tonight?</h1>
-      <button onClick={handleClick} className="value">
-        {state ? "Yes" : "No"}
+      <button
+        onClick={handleClick}
+        className="value"
+        aria-label={`The current decision is set to ${
+          isGoingOut ? "Yes" : "No"
+        }, click again to change the decision!`}
+      >
+        {isGoingOut ? "Yes" : "No"}
       </button>
     </main>
   );
