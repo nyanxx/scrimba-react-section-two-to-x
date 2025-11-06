6574543fa389923e965717478f4136a8ce1db598
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-06 11:34:24 +0530

    myadd: useState - State Practice Challenge 1

--- section-three-react-state/practice-exercise/useState/App.jsx
+++ section-three-react-state/practice-exercise/useState/App.jsx
@@ -6,15 +6,40 @@ export default function App() {
    * Create state to track our count value (initial value is 0)
    * Don't forget to replace the hard-coded "0" with your new state
    */
+
+  let [count, setCount] = React.useState(0);
+
+  function handleClickAdd() {
+    setCount(() => {
+      count++;
+      return count;
+    });
+  }
+
+  function handleClickSub() {
+    setCount(() => {
+      count--;
+      return count;
+    });
+  }
+
   return (
     <main className="container">
       <h1>How many times will Bob say "state" in this section?</h1>
       <div className="counter">
-        <button className="minus" aria-label="Decrease count">
+        <button
+          onClick={handleClickSub}
+          className="minus"
+          aria-label="Decrease count"
+        >
           -
         </button>
-        <h2 className="count">0</h2>
-        <button className="plus" aria-label="Increase count">
+        <h2 className="count">{count}</h2>
+        <button
+          onClick={handleClickAdd}
+          className="plus"
+          aria-label="Increase count"
+        >
           +
         </button>
       </div>
