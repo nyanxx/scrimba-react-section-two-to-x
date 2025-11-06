3c67c1ddac7949f691adddc92f02c675d9731c0e
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-06 12:42:34 +0530

    done: useState - State Practice Challenge 1 2 & 3

--- section-three-react-state/practice-exercise/useState/App.jsx
+++ section-three-react-state/practice-exercise/useState/App.jsx
@@ -5,15 +5,22 @@ export default function App() {
    * useState - State Practice Challenge 1:
    * Create state to track our count value (initial value is 0)
    * Don't forget to replace the hard-coded "0" with your new state
+   *
+   * useState - State Practice Challenge 2:
+   * Create a function called `add` that runs
+   * when the + button is clicked. (Can just console.log("add") for now)
+   *
+   * useState - State Practice Challenge 3:
+   * See if you can think of a way to add 1 to the count
+   * every time the + button is clicked
+   *
    */
 
-  let [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(0);
 
-  function handleClickAdd() {
-    setCount(() => {
-      count++;
-      return count;
-    });
+  function add() {
+    setCount(count + 1);
+    // using ++ or -- is a bin no no in React!
   }
 
   function handleClickSub() {
@@ -35,11 +42,7 @@ export default function App() {
           -
         </button>
         <h2 className="count">{count}</h2>
-        <button
-          onClick={handleClickAdd}
-          className="plus"
-          aria-label="Increase count"
-        >
+        <button onClick={add} className="plus" aria-label="Increase count">
           +
         </button>
       </div>
