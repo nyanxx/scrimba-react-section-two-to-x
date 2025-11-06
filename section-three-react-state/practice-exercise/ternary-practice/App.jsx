5569d46299df762473b8bc7036f17adbd757add6
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-06 15:27:40 +0530

    done: Ternary practice

--- section-three-react-state/practice-exercise/ternary-practice/App.jsx
+++ section-three-react-state/practice-exercise/ternary-practice/App.jsx
@@ -1,23 +1,31 @@
 export default function App() {
-    /**
-     * Challenge: Replace the if/else below with a ternary
-     * to determine the text that should display on the page
-     */
-    const isGoingOut = true
-    
-    let answer  // 👈 Use ternary here
-    
-    // Remove the code below 👇 once your ternary is done
-    if(isGoingOut === true) {
-        answer = "Yes"
-    } else {
-        answer = "No"
-    }
-    
-    return (
-        <main>
-            <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value">{answer}</button>
-        </main>
-    )
+  /**
+   * Ternary practice Challenge 1: Replace the if/else below with a ternary
+   * to determine the text that should display on the page
+   */
+
+  /**
+   * Ternary practice Challenge 2: move our ternary directly inside of the JSX
+   * so the "Yes" and "No" are determined inside the <h1>
+   *
+   * Hint: you will no longer need the `answer` variable
+   */
+
+  const isGoingOut = true;
+
+  //   const answer = isGoingOut ? "Yes" : "No"; // 👈 Use ternary here
+
+  //   // Remove the code below 👇 once your ternary is done
+  //   if (isGoingOut === true) {
+  //     answer = "Yes";
+  //   } else {
+  //     answer = "No";
+  //   }
+
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button className="value">{isGoingOut ? "Yes" : "No"}</button>
+    </main>
+  );
 }
