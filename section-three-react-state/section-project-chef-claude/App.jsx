a72bec87d6b3660729823beb0c35024eb73c6464
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-15 06:13:38 +0530

    refactor

--- section-three-react-state/section-project-chef-claude/App.jsx
+++ section-three-react-state/section-project-chef-claude/App.jsx
@@ -5,20 +5,14 @@
 
 import Header from "./components/Header";
 import Main from "./components/Main";
-// import AddIngredient from "./components/AddIngredient";
-import IngredientOnHands from "./components/IngredientOnHands";
-import GetRecipe from "./components/GetRecipe";
-import ShowRecipe from "./components/ShowRecipe";
+// import ShowRecipe from "./components/ShowRecipe";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
-      {/* <AddIngredient /> */}
-      <IngredientOnHands />
-      <GetRecipe />
-      <ShowRecipe />
+      {/* <ShowRecipe /> */}
     </>
   );
 }
--- section-three-react-state/section-project-chef-claude/components/Main.jsx
+++ section-three-react-state/section-project-chef-claude/components/Main.jsx
@@ -1,9 +1,57 @@
-import AddIngredient from "./AddIngredient";
+import { useState } from "react";
 
 export default function Main() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const listIngredients = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(formData) {
+    setIngredients((prevIngredients) => {
+      return [...prevIngredients, formData.get("ingredient")];
+    });
+  }
+
   return (
     <main>
-      <AddIngredient />
+      <form className="add-ingredient" action={addIngredient}>
+        <input
+          id="input-ingredient"
+          name="ingredient"
+          aria-label="Enter ingredient"
+          type="text"
+          className="input-ingredient"
+          placeholder="e.g. oregano"
+        />
+        <button
+          id="btn-add-ingredient"
+          aria-label="Add ingredient"
+          name="add-ingredient"
+          className="btn-add-ingredient"
+        >
+          Add ingredient
+        </button>
+      </form>
+      <ul>{listIngredients}</ul>
+      <section className="ingredient-on-hands">
+        <h2>Ingredient on hand:</h2>
+        <ul>
+          <li>item x</li>
+          <li>item x</li>
+          <li>item x</li>
+          <li>item x</li>
+          <li>item x</li>
+          <li>item x</li>
+        </ul>
+      </section>
+      <section className="get-recipe">
+        <div>
+          <h3>Ready for a recipe?</h3>
+          <p>Generate a recipe from your list of ingredients</p>
+        </div>
+        <button>Get a recipe</button>
+      </section>
     </main>
   );
 }
