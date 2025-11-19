fa98420f9d217266aa6bb1e788613b752e23f408
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-19 10:14:51 +0530

    myadd: Sound pads challenge, part 1

---
+++ section-three-react-state/practice-exercise/sound-pad-challenge/App.jsx
@@ -0,0 +1,30 @@
+import padsData from "./pads";
+import React from "react";
+
+export default function App() {
+  /**
+   * Sound pads challenge, part 1:
+   * 1. Initialize state with the default value of the
+   *    array pulled in from pads.js
+   * 2. Map over that state array and display each one
+   *    as a <button> (CSS ids already written for you)
+   *    (Don't worry about using the "on" or "color"
+   *    properties yet)
+   */
+
+  const [pads, setPadsArr] = React.useState([padsData]);
+
+  const padsRender = pads.map((pad) => {
+    return (
+      <button key={pad.id} style={{ background: pad.color }}>
+        {pad.id}
+      </button>
+    );
+  });
+
+  return (
+    <main>
+      <div className="pad-container">{padsRender}</div>
+    </main>
+  );
+}
