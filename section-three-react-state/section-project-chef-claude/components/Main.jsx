d8bc8ade96723760f3a56ca2687b6e9b94846d97
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-16 19:30:15 +0530

    section project chef-claude completed

--- section-three-react-state/section-project-chef-claude/components/Main.jsx
+++ section-three-react-state/section-project-chef-claude/components/Main.jsx
@@ -3,23 +3,6 @@ import IngredientList from "./IngredientList";
 import GetRecipe from "./GetRecipe";
 import { getRecipeFromMistral } from "../ai";
 
-/**
- * Challenge: Get a recipe from the AI! [√]
- *
- * This will be a bit harder of a challenge that will require you
- * to think critically and synthesize the skills you've been
- * learning and practicing up to this point.
- *
- * Using either the `getRecipeFromChefClaude` function or the
- * `getRecipeFromMistral` [√] function, make it so that when the user
- * clicks "Get a recipe", the text response from the AI is displayed
- * in the <ClaudeRecipe> component.
- *
- * For now, just have it render the raw markdown that the AI returns,
- * don't worry about making it look nice yet. (We're going to use a
- * package that will render the markdown for us soon.)
- */
-
 export default function Main() {
   const [ingredients, setIngredients] = useState([]);
 
@@ -30,14 +13,6 @@ export default function Main() {
         })
       : console.log("Add some ingredients");
   }
-
-  // const [recipeShown, setRecipeShown] = useState(false);
-
-  // function toggleRecipeShown() {
-  //   setRecipeShown((prevState) => {
-  //     return !prevState;
-  //   });
-  // }
 
   return (
     <main>
@@ -62,7 +37,6 @@ export default function Main() {
       {ingredients.length > 0 && <IngredientList ingredients={ingredients} />}
       {ingredients.length > 3 && (
         <GetRecipe
-          // toggleRecipeShown={toggleRecipeShown}
           getRecipeFromMistral={getRecipeFromMistral}
           ingredients={ingredients}
         />
