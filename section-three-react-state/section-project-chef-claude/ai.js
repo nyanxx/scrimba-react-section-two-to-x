002abab483a55b8c26d7db06d8e7b26b045949c0
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-16 17:02:50 +0530

    feat: update system prompt to get desired response

--- section-three-react-state/section-project-chef-claude/ai.js
+++ section-three-react-state/section-project-chef-claude/ai.js
@@ -6,7 +6,24 @@ const client = new InferenceClient(import.meta.env.VITE_HF_ACCESS_TOKEN, {
 
 export async function getRecipeFromMistral(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(", ");
-  const systemPrompt = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`;
+  const systemPrompt = `
+  You are an assistant that receives a list of ingredients that a user has 
+  and suggests a recipe they could make with some or all of those ingredients.
+  You don't need to use every ingredient they mention in your recipe. 
+  The recipe can include additional ingredients they didn't mention, 
+  but try not to include too many extra ingredients. 
+  Format response with the format given below same to same especially the hashtags to make it easier to render to a web page.
+  Below is an example format -
+  ## Dish Name
+
+  ### Ingredients:
+
+  - List of ingredients..
+
+  ### Procedures:
+
+  1. Steps / Procedures...
+  `;
   const userPrompt = `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`;
   try {
     console.log("Getting recipe from Mistral AI...");
@@ -24,6 +41,7 @@ export async function getRecipeFromMistral(ingredientsArr) {
       ],
       max_new_tokens: 1024,
       temperature: 1.1,
+      stop: ["```", "markdown"],
     });
 
     console.log("From ai.js > ", response);
