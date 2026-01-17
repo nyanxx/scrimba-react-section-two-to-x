6b960a3c3367be4d238f5c3547b992c7b7d6b5e3
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-17 11:01:23 +0530

    done: Meme Generator State Challenge

--- section-four-side-effects/section-project-meme-generator/components/Main.jsx
+++ section-four-side-effects/section-project-meme-generator/components/Main.jsx
@@ -1,22 +1,38 @@
+import React from "react";
+
 export default function Main() {
+  /**
+   * Meme Generator State Challenge:
+   * move the hardcoded meme info into React
+   * state. Use an object with `topText`, `bottomText`,
+   * and `imageUrl` properties, and set the initial values to
+   * the ones hardcoded below.
+   */
+
+  const [meme, setMeme] = React.useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input type="text" placeholder={meme.topText} name="topText" />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input type="text" placeholder={meme.bottomText} name="bottomText" />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
