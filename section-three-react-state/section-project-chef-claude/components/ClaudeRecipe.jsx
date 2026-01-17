8068caa03981bd58bb67aee8be61e2a32f0d9152
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-17 10:11:45 +0530

    chore: add aria-live and some css

--- section-three-react-state/section-project-chef-claude/components/ClaudeRecipe.jsx
+++ section-three-react-state/section-project-chef-claude/components/ClaudeRecipe.jsx
@@ -2,7 +2,8 @@ import ReactMarkdown from "react-markdown";
 
 export default function ClaudeRecipe(props) {
   return (
-    <section className="show-recipe">
+    <section className="show-recipe" aria-live="polite">
+      <h2>Chef Claude Recommends:</h2>
       <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
