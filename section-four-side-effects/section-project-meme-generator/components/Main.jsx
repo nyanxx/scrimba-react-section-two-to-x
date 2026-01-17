3c0d94f6dded6e2ec0b38b6199d2419e3a9b9d55
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-17 11:24:11 +0530

    myadd: Controlled Components Challenge - part 1

--- section-four-side-effects/section-project-meme-generator/components/Main.jsx
+++ section-four-side-effects/section-project-meme-generator/components/Main.jsx
@@ -1,31 +1,55 @@
 import React from "react";
 
 export default function Main() {
-  /**
-   * Meme Generator State Challenge:
-   * move the hardcoded meme info into React
-   * state. Use an object with `topText`, `bottomText`,
-   * and `imageUrl` properties, and set the initial values to
-   * the ones hardcoded below.
-   */
-
   const [meme, setMeme] = React.useState({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
 
+  function handleChange(event) {
+    /**
+     * Controlled Components Challenge - part 1 : update the topText value in the meme state
+     * object every time the topText input box is changed
+     *
+     * Note: don't worry about bottomText at this point.
+     */
+    setMeme((prevObj) => {
+      if (event.target.name === "topText") {
+        return {
+          ...prevObj,
+          topText: event.target.value,
+        };
+      } else if (event.target.name === "bottomText") {
+        return {
+          ...prevObj,
+          bottomText: event.target.value,
+        };
+      }
+    });
+  }
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder={meme.topText} name="topText" />
+          <input
+            type="text"
+            placeholder={meme.topText}
+            name="topText"
+            onChange={handleChange}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder={meme.bottomText} name="bottomText" />
+          <input
+            type="text"
+            placeholder={meme.bottomText}
+            name="bottomText"
+            onChange={handleChange}
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
