9c7440345a045c533ce55a28a9f26142e03a0bfd
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-18 11:31:43 +0530

    add: State and Effect Practices

---
+++ section-four-side-effects/practice/state-and-effect/App.jsx
@@ -0,0 +1,17 @@
+import WindowTracker from "./WindowTracker";
+
+export default function App() {
+  /**
+   * State and Effect Practices:
+   * 1. Create state called `show`, default to `true`
+   * 2. When the button is clicked, toggle `show`
+   * 3. Only display `<WindowTracker>` if `show` is `true`
+   */
+
+  return (
+    <main className="container">
+      <button>Toggle WindowTracker</button>
+      <WindowTracker />
+    </main>
+  );
+}
